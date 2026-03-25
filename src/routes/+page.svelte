dd85383ff4e1ca7965fc476a7e42aaa0be8f318e
Peter John Rueca <peter.rueca@safetychampion.online>
2026-03-26 01:00:17 +0800

    wip: partial implementation of FreezerDetails component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,12 @@
     import Navbar from './components/Navbar.svelte'
     import CabCon from './components/CabCon.svelte'
     import FreezerList from './components/FreezerList.svelte'
+    import FreezerDetails from './components/FreezerDetails.svelte'
 </script>
 
 <div>
     <Navbar />
     <CabCon />
     <FreezerList />
+    <FreezerDetails />
 </div>
---
+++ src/routes/components/FreezerDetails.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import Icon from '@iconify/svelte'
+
+    let open = $state(false)
+</script>
+
+<!-- Open the modal using ID.showModal() method -->
+<dialog id="freezer-details" class="modal" {open}>
+    <div class="modal-box">
+        <form method="dialog">
+            <button class="close-btn btn btn-circle btn-ghost">
+                <Icon icon="solar:close-square-bold" width="24" />
+            </button>
+        </form>
+        <h3 class="text-lg font-bold">Hello!</h3>
+        <p class="py-4">Press ESC key or click outside to close</p>
+    </div>
+    <form method="dialog" class="modal-backdrop">
+        <button>close</button>
+    </form>
+</dialog>
+
+<style lang="postcss">
+    @reference 'tailwindcss';
+
+    .close-btn {
+        @apply absolute top-2 right-2;
+    }
+</style>
